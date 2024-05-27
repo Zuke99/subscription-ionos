118dc85c9e71fc75e0f1a0895cf33c9689faa1fb
Dheeraj P <dheeruzuke99@gmail.com>
2024-05-27 20:56:55 +0530

    title

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const UserService = require("../services/userService");
 const registerUser = async (req, res) => {
   try {
     const result = await UserService.addUser(req.body);
-    return res.status(200).json({ status: "success", message: "User Registered Successfully", data: result });
+    return res.status(200).json({ status: "success", message: "User Registered, Login With Same Credentials", data: result });
   } catch (err) {
     return res.status(500).json({ status: "error", message: err });
   }
